582051d4db829b8ec265e12695be87d126eafcab
Redgorila <agmintroap@gmail.com>
2023-12-24 00:25:04 +0100

    connection to local db

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,20 @@ import { PaymentModule } from './payment/payment.module'
 import { CarModule } from './car/car.module'
 import { AuthModule } from './auth/auth.module'
 import { DatabaseModule } from './database/database.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'antonio2',
+      password: 'password',
+      database: 'antonio_sells_cars',
+      entities: [],
+      synchronize: true,
+    }),
     UserModule,
     SaleModule,
     PaymentModule,
